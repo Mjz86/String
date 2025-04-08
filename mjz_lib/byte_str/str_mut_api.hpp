4b6d4797157ef5b8a00b745742c31ff3cafcd8b8
mjz <112626877+Mjz86@users.noreply.github.com>
2025-04-08 15:26:18 +0330

    Update str_mut_api.hpp

--- mjz_lib/byte_str/str_mut_api.hpp
+++ mjz_lib/byte_str/str_mut_api.hpp
@@ -233,13 +233,25 @@ MJZ_CX_AL_FN void basic_str_t<version_v, has_alloc_v_>::reset_to_error_on_fail(
           encodings_e(view->encodings) == encodings_e::ascii ||
               encodings_e(view->encodings) == encodings_e::err_ascii);
   if (op) MJZ_MOSTLY_LIKELY return;
-  [view,this]()noexcept {
-    asserts(asserts.assume_rn, m.deconstruct_to_invalid());
-    asserts(asserts.assume_rn,
-            m.construct_non_sso_from_invalid(view->ptr, view->len, nullptr, 0,
-                                             view->is_static, false));
-    m.d_set_cntrl(my_details::encodings_bits, encodings_e::err_ascii);
-  }();
+  if constexpr (MJZ_IN_DEBUG_MODE) {
+    [&]() noexcept {
+      asserts(asserts.assume_rn, m.deconstruct_to_invalid());
+      asserts(asserts.assume_rn,
+              m.construct_non_sso_from_invalid(view->ptr, view->len, nullptr, 0,
+                                               view->is_static, false));
+      m.d_set_cntrl(my_details::encodings_bits, encodings_e::err_ascii);
+    }();
+  } else{
+    [&]() noexcept {
+      static_string_view view_ = "[Err]";
+      asserts(asserts.assume_rn, m.deconstruct_to_invalid());
+      asserts(asserts.assume_rn,
+              m.construct_non_sso_from_invalid(view_->ptr, view_->len, nullptr,
+                                               0, view_->is_static, false));
+      m.d_set_cntrl(my_details::encodings_bits, encodings_e::err_ascii);
+    }();
+  }
+  
 }
 template <version_t version_v, bool has_alloc_v_>
 MJZ_CX_FN success_t basic_str_t<version_v, has_alloc_v_>::copy_assign_data_fast(
