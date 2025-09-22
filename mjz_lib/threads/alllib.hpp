26a44d86adc988b4a505bf28678d0d27d0e16102
mjz <112626877+Mjz86@users.noreply.github.com>
2025-09-22 22:00:22 +0330

    Update alllib.hpp

--- mjz_lib/threads/alllib.hpp
+++ mjz_lib/threads/alllib.hpp
@@ -4,13 +4,15 @@
 #include "big_mutext.hpp"
 #include "bit_mutex.hpp"
 #include "cx_atomic_ref.hpp"
-#include "jthread.hpp"
 #include "lock_guard.hpp"
 #include "mutex_ref.hpp"
 
 // optional file
 #if __has_include("recursive_mutex.hpp")
 #include "recursive_mutex.hpp"
+#endif
+#if __has_include("jthread.hpp")
+#include "jthread.hpp"
 #endif
 
 #if __has_include("counting_semaphore.hpp")
